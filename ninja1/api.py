247a9b68734db3bcc7e6c8c0b34f5aba4bf67102
ruthromerocarretero <rromerocarretero.cf@iesesteveterradas.cat>
2026-03-24 13:34:52 +0100

    finished django ninja

--- ninja1/api.py
+++ ninja1/api.py
@@ -27,15 +27,34 @@ class EmployeeOut(Schema):
 def hello(request):
     return "Hello world"
 
-@api.get("/employees", response=List[EmployeeOut])
-def list_employees(request):
-    return Employee.objects.all()
-
 @api.get("/employees/{employee_id}", response=EmployeeOut)
 def get_employee(request, employee_id: int):
-    return get_object_or_404(Employee, id=employee_id)
+    employee = get_object_or_404(Employee, id=employee_id)
+    return employee
 
-@api.post("/employees")
+
+@api.get("/employees", response=List[EmployeeOut])
+def list_employees(request):
+    qs = Employee.objects.all()
+    return qs
+
+
+@api.put("/employees/{employee_id}")
+def update_employee(request, employee_id: int, payload: EmployeeIn):
+    employee = get_object_or_404(Employee, id=employee_id)
+    for attr, value in payload.dict().items():
+        setattr(employee, attr, value)
+    employee.save()
+    return {"success": True}
+
+
+@api.delete("/employees/{employee_id}")
+def delete_employee(request, employee_id: int):
+    employee = get_object_or_404(Employee, id=employee_id)
+    employee.delete()
+    return {"success": True}
+
+@api.post("/employees", response=EmployeeOut)
 def create_employee(request, payload: EmployeeIn):
     employee = Employee.objects.create(**payload.dict())
-    return {"id": employee.id}
+    return employee
